fa5992ca60ba38445342c71faea24691dca35b7a
Mohamed El mouhib <mel-mouh@e2r11p11.1337.ma>
2025-03-13 21:11:55 +0000

    added

---
+++ libftl/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-mouh <mel-mouh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 14:26:51 by mel-mouh          #+#    #+#             */
+/*   Updated: 2024/12/24 17:40:30 by mel-mouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int			i;
+	long int	numbers;
+	int			sign;
+
+	i = 0;
+	numbers = 0;
+	sign = 1;
+	if (!str)
+		return (0);
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '+')
+			;
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		numbers = numbers * 10 + (str[i] - '0');
+		i++;
+	}
+	return (numbers * sign);
+}
